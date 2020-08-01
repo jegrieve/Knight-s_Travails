72595b5e1e6a384150f63c8873801ec194f70866
James Grieve <pocojames@hotmail.com>
2020-07-31 21:48:21 -0700

    complete possible_moves method for knight class

---
+++ knight.rb
@@ -0,0 +1,22 @@
+class Knight
+    def initialize(position)
+        @position = position
+        @parent = nil
+        @children = []
+    end
+
+    def possible_moves(position) #Ex: [3,3]
+        knight_moves = [[1,2],[-1,2],[-1,-2],[1,-2],[2,-1],[2,1],[-2,-1],[-2,1]]
+        knight_placements = []
+        x,y = position
+
+        knight_moves.each do |pos|
+            j, i = pos
+            knight_placements << [j + x, i + y]
+        end
+        knight_placements
+    end
+end
+a = Knight.new([3,3])
+p a.possible_moves([3,3])
+
